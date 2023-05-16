d0333fee18b7872b4ea01d976556c1d721b61c82
Jaime <jaimediaz.pro@gmail.com>
2023-05-16 10:25:58 +0200

    clean dependencies and others

--- src/test/java/com/test/inditex/infrastructure/inputadapter/http/PriceAPITest.java
+++ src/test/java/com/test/inditex/infrastructure/inputadapter/http/PriceAPITest.java
@@ -1,11 +1,9 @@
 package com.test.inditex.infrastructure.inputadapter.http;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -14,14 +12,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
 class PriceAPITest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void test1() throws Exception {
+    void test1() throws Exception {
         mockMvc.perform(get("/price/get")
                         .param("effectiveDate", "2020-06-14 10:00:00")
                         .param("productId", "35455")
@@ -36,7 +33,7 @@ class PriceAPITest {
     }
 
     @Test
-    public void test2() throws Exception {
+    void test2() throws Exception {
         mockMvc.perform(get("/price/get")
                         .param("effectiveDate", "2020-06-14 16:00:00")
                         .param("productId", "35455")
@@ -51,7 +48,7 @@ class PriceAPITest {
     }
 
     @Test
-    public void test3() throws Exception {
+    void test3() throws Exception {
         mockMvc.perform(get("/price/get")
                         .param("effectiveDate", "2020-06-14 21:00:00")
                         .param("productId", "35455")
@@ -66,7 +63,7 @@ class PriceAPITest {
     }
 
     @Test
-    public void test4() throws Exception {
+    void test4() throws Exception {
         mockMvc.perform(get("/price/get")
                         .param("effectiveDate", "2020-06-15 10:00:00")
                         .param("productId", "35455")
@@ -81,7 +78,7 @@ class PriceAPITest {
     }
 
     @Test
-    public void test5() throws Exception {
+    void test5() throws Exception {
         mockMvc.perform(get("/price/get")
                         .param("effectiveDate", "2020-06-16 21:00:00")
                         .param("productId", "35455")
